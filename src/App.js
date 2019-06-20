b60a1a499d42fec416e0fd1d9baeb2b283ff920c
Vishal <vishal.sharma@cloudanalogy.com>
2019-06-20 15:38:31 +0530

    Adding two way binding

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = props => {
+
+   const [personsState, setPersonsState ] = useState({
+        persons:[
+            {name : 'Vishu' , age: 28},
+            {name : 'Suraj' , age: 27},
+            {name : 'Max' , age: 29}
+        ],
+
+
+   });
+
+   //const [otherState, setOtherState]= useState('some other value');
+
+ const switchNameHandler = (newName) => {
+     console.log('clicked');
+     setPersonsState({persons:[
+             {name : newName , age: 28},
+             {name : 'Suraj' , age: 27},
+             {name : 'Max' , age: 39}
+         ]
+     } )
+ }
+        return (
+            <div className="App">
+                <h1>Hi, I'm a React App</h1>
+                <button onClick={switchNameHandler.bind(this, 'Vishu123')}>Switch Name</button>
+                <Person
+                    name={personsState.persons[0].name}
+                    age ={personsState.persons[0].age} />
+                <Person
+                    name={personsState.persons[1].name}
+                    age ={personsState.persons[1].age}
+                    click={switchNameHandler.bind(this,'Vishu 45')}>My Hobby - Racing</Person>
+                <Person
+                    name={personsState.persons[2].name}
+                    age ={personsState.persons[2].age} />
+            </div>
+        );
+ //return React.createElement('div',{className: 'App'},React.createElement('h1',null,'I am React app'));
+};
 
 export default App;
+
+/*
+state = {
+    persons:[
+        {name : 'Vishu' , age: 28},
+        {name : 'Suraj' , age: 27},
+        {name : 'Max' , age: 29}
+    ]
+}
+
+switchNameHandler = () => {
+    console.log('clicked');
+    this.setState({persons:[
+            {name : 'Vishu 1' , age: 28},
+            {name : 'Suraj' , age: 27},
+            {name : 'Max' , age: 39}
+        ]
+    } )
+}*/
